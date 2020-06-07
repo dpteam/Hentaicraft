99a3b9c01f5402ed202a57670a2d20ad0498034c
DartPower <SIMPSON-NEKEK@mail.ru>
2020-06-07 20:51:34 +0300

    Added a fluid, prepare to ukr lang

---
+++ src/main/java/io/alaska/nsfw/block/BlockVulgariumFluid.java
@@ -0,0 +1,91 @@
+
+package io.alaska.nsfw.block;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.BlockFluidClassic;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.gen.feature.WorldGenLakes;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.model.ModelBakery;
+import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.Block;
+
+import java.util.Random;
+
+import io.alaska.nsfw.ElementsNSFW;
+
+@ElementsNSFW.ModElement.Tag
+public class BlockVulgariumFluid extends ElementsNSFW.ModElement {
+	@GameRegistry.ObjectHolder("nsfw:vulgariumfluid")
+	public static final Block block = null;
+	@GameRegistry.ObjectHolder("nsfw:vulgariumfluid")
+	public static final Item item = null;
+	private Fluid fluid;
+	public BlockVulgariumFluid(ElementsNSFW instance) {
+		super(instance, 3);
+		fluid = new Fluid("vulgariumfluid", new ResourceLocation("nsfw:blocks/vulgariumfluid_still"),
+				new ResourceLocation("nsfw:blocks/vulgariumfluid_flow")).setLuminosity(50).setDensity(1000).setViscosity(1000).setGaseous(false);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockFluidClassic(fluid, Material.WATER) {
+		}.setUnlocalizedName("vulgariumfluid").setRegistryName("vulgariumfluid"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName("vulgariumfluid"));
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		FluidRegistry.registerFluid(fluid);
+		FluidRegistry.addBucketForFluid(fluid);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerModels(ModelRegistryEvent event) {
+		ModelBakery.registerItemVariants(item);
+		ModelLoader.setCustomMeshDefinition(item, new ItemMeshDefinition() {
+			@Override
+			public ModelResourceLocation getModelLocation(ItemStack stack) {
+				return new ModelResourceLocation("nsfw:vulgariumfluid", "vulgariumfluid");
+			}
+		});
+		ModelLoader.setCustomStateMapper(block, new StateMapperBase() {
+			@Override
+			protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+				return new ModelResourceLocation("nsfw:vulgariumfluid", "vulgariumfluid");
+			}
+		});
+	}
+
+	@Override
+	public void generateWorld(Random random, int chunkX, int chunkZ, World world, int dimID, IChunkGenerator cg, IChunkProvider cp) {
+		boolean dimensionCriteria = false;
+		if (dimID == 0)
+			dimensionCriteria = true;
+		if (!dimensionCriteria)
+			return;
+		int i = chunkX + random.nextInt(16) + 8;
+		int j = random.nextInt(256);
+		int k = chunkZ + random.nextInt(16) + 8;
+		new WorldGenLakes(block).generate(world, random, new BlockPos(i, j, k));
+	}
+}
