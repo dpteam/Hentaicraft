3ae23c2cbb75fc42dd73fe3b89f48807c4dc76c1
DartPower <SIMPSON-NEKEK@mail.ru>
2020-06-07 23:07:38 +0300

    LOL

---
+++ src/main/java/io/alaska/nsfw/entity/EntityAdam.java
@@ -0,0 +1,177 @@
+
+package io.alaska.nsfw.entity;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.common.DungeonHooks;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.DamageSource;
+import net.minecraft.item.Item;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAILeapAtTarget;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.RenderBiped;
+import net.minecraft.client.model.ModelBiped;
+
+import java.util.Random;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+import io.alaska.nsfw.ElementsNSFW;
+
+@ElementsNSFW.ModElement.Tag
+public class EntityAdam extends ElementsNSFW.ModElement {
+	public static final int ENTITYID = 3;
+	public static final int ENTITYID_RANGED = 4;
+	public EntityAdam(ElementsNSFW instance) {
+		super(instance, 8);
+	}
+
+	@Override
+	public void initElements() {
+		elements.entities.add(() -> EntityEntryBuilder.create().entity(EntityCustom.class).id(new ResourceLocation("nsfw", "adam"), ENTITYID)
+				.name("adam").tracker(64, 3, true).egg(-13421569, -16777012).build());
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+		Biome[] spawnBiomes = allbiomes(Biome.REGISTRY);
+		EntityRegistry.addSpawn(EntityCustom.class, 20, 3, 30, EnumCreatureType.MONSTER, spawnBiomes);
+		DungeonHooks.addDungeonMob(new ResourceLocation("nsfw:adam"), 180);
+	}
+
+	private Biome[] allbiomes(net.minecraft.util.registry.RegistryNamespaced<ResourceLocation, Biome> in) {
+		Iterator<Biome> itr = in.iterator();
+		ArrayList<Biome> ls = new ArrayList<Biome>();
+		while (itr.hasNext())
+			ls.add(itr.next());
+		return ls.toArray(new Biome[ls.size()]);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		RenderingRegistry.registerEntityRenderingHandler(EntityCustom.class, renderManager -> {
+			RenderBiped customRender = new RenderBiped(renderManager, new ModelBiped(), 0.5f) {
+				protected ResourceLocation getEntityTexture(Entity entity) {
+					return new ResourceLocation("nsfw:textures/adam.png");
+				}
+			};
+			customRender.addLayer(new net.minecraft.client.renderer.entity.layers.LayerBipedArmor(customRender) {
+				protected void initArmor() {
+					this.modelLeggings = new ModelBiped(0.5f);
+					this.modelArmor = new ModelBiped(1);
+				}
+			});
+			return customRender;
+		});
+	}
+	public static class EntityCustom extends EntityMob {
+		public EntityCustom(World world) {
+			super(world);
+			setSize(0.6f, 1.8f);
+			experienceValue = 5;
+			this.isImmuneToFire = false;
+			setNoAI(!true);
+			setCustomNameTag("Adam");
+			setAlwaysRenderNameTag(true);
+		}
+
+		@Override
+		protected void initEntityAI() {
+			super.initEntityAI();
+			this.tasks.addTask(1, new EntityAIWander(this, 1));
+			this.tasks.addTask(2, new EntityAILookIdle(this));
+			this.tasks.addTask(3, new EntityAISwimming(this));
+			this.tasks.addTask(4, new EntityAILeapAtTarget(this, (float) 0.8));
+			this.tasks.addTask(5, new EntityAIPanic(this, 1.2));
+			this.targetTasks.addTask(6, new EntityAIHurtByTarget(this, false));
+		}
+
+		@Override
+		public EnumCreatureAttribute getCreatureAttribute() {
+			return EnumCreatureAttribute.UNDEFINED;
+		}
+
+		@Override
+		protected Item getDropItem() {
+			return null;
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getAmbientSound() {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("block.anvil.land"));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getHurtSound(DamageSource ds) {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("entity.generic.hurt"));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getDeathSound() {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("entity.generic.death"));
+		}
+
+		@Override
+		protected float getSoundVolume() {
+			return 1.0F;
+		}
+
+		@Override
+		public boolean attackEntityFrom(DamageSource source, float amount) {
+			if (source == DamageSource.FALL)
+				return false;
+			if (source == DamageSource.CACTUS)
+				return false;
+			if (source == DamageSource.DROWN)
+				return false;
+			return super.attackEntityFrom(source, amount);
+		}
+
+		@Override
+		protected void applyEntityAttributes() {
+			super.applyEntityAttributes();
+			if (this.getEntityAttribute(SharedMonsterAttributes.ARMOR) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(0D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(10D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3D);
+		}
+
+		public void onLivingUpdate() {
+			super.onLivingUpdate();
+			int i = (int) this.posX;
+			int j = (int) this.posY;
+			int k = (int) this.posZ;
+			Random random = this.rand;
+			if (true)
+				for (int l = 0; l < 4; ++l) {
+					double d0 = (i + 0.5) + (random.nextFloat() - 0.5) * 0.5D;
+					double d1 = ((j + 0.7) + (random.nextFloat() - 0.5) * 0.5D * 100) + 0.5D;
+					double d2 = (k + 0.5) + (random.nextFloat() - 0.5) * 0.5D;
+					world.spawnParticle(EnumParticleTypes.HEART, d0, d1, d2, 0, 0, 0);
+				}
+		}
+	}
+}
---
+++ src/main/java/io/alaska/nsfw/entity/EntityVulgariumMan.java
@@ -0,0 +1,169 @@
+
+package io.alaska.nsfw.entity;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.common.DungeonHooks;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAILeapAtTarget;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.RenderBiped;
+import net.minecraft.client.model.ModelBiped;
+
+import java.util.Random;
+
+import io.alaska.nsfw.block.BlockVulgariumGrassBlock;
+import io.alaska.nsfw.ElementsNSFW;
+
+@ElementsNSFW.ModElement.Tag
+public class EntityVulgariumMan extends ElementsNSFW.ModElement {
+	public static final int ENTITYID = 1;
+	public static final int ENTITYID_RANGED = 2;
+	public EntityVulgariumMan(ElementsNSFW instance) {
+		super(instance, 7);
+	}
+
+	@Override
+	public void initElements() {
+		elements.entities.add(() -> EntityEntryBuilder.create().entity(EntityCustom.class).id(new ResourceLocation("nsfw", "vulgariumman"), ENTITYID)
+				.name("vulgariumman").tracker(64, 3, true).egg(-52225, -65281).build());
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+		Biome[] spawnBiomes = {Biome.REGISTRY.getObject(new ResourceLocation("nsfw:vulgariumbiome0")),};
+		EntityRegistry.addSpawn(EntityCustom.class, 20, 3, 30, EnumCreatureType.MONSTER, spawnBiomes);
+		DungeonHooks.addDungeonMob(new ResourceLocation("nsfw:vulgariumman"), 180);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		RenderingRegistry.registerEntityRenderingHandler(EntityCustom.class, renderManager -> {
+			RenderBiped customRender = new RenderBiped(renderManager, new ModelBiped(), 0.5f) {
+				protected ResourceLocation getEntityTexture(Entity entity) {
+					return new ResourceLocation("nsfw:textures/vulgariumman_old.png");
+				}
+			};
+			customRender.addLayer(new net.minecraft.client.renderer.entity.layers.LayerBipedArmor(customRender) {
+				protected void initArmor() {
+					this.modelLeggings = new ModelBiped(0.5f);
+					this.modelArmor = new ModelBiped(1);
+				}
+			});
+			return customRender;
+		});
+	}
+	public static class EntityCustom extends EntityMob {
+		public EntityCustom(World world) {
+			super(world);
+			setSize(0.6f, 1.8f);
+			experienceValue = 5;
+			this.isImmuneToFire = false;
+			setNoAI(!true);
+			setCustomNameTag("VulgariumMan");
+			setAlwaysRenderNameTag(true);
+		}
+
+		@Override
+		protected void initEntityAI() {
+			super.initEntityAI();
+			this.tasks.addTask(1, new EntityAIWander(this, 1));
+			this.tasks.addTask(2, new EntityAILookIdle(this));
+			this.tasks.addTask(3, new EntityAISwimming(this));
+			this.tasks.addTask(4, new EntityAILeapAtTarget(this, (float) 0.8));
+			this.tasks.addTask(5, new EntityAIPanic(this, 1.2));
+			this.targetTasks.addTask(6, new EntityAIHurtByTarget(this, false));
+		}
+
+		@Override
+		public EnumCreatureAttribute getCreatureAttribute() {
+			return EnumCreatureAttribute.UNDEFINED;
+		}
+
+		@Override
+		protected Item getDropItem() {
+			return new ItemStack(BlockVulgariumGrassBlock.block, (int) (1)).getItem();
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getAmbientSound() {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("block.anvil.land"));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getHurtSound(DamageSource ds) {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("entity.generic.hurt"));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getDeathSound() {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("entity.generic.death"));
+		}
+
+		@Override
+		protected float getSoundVolume() {
+			return 1.0F;
+		}
+
+		@Override
+		public boolean attackEntityFrom(DamageSource source, float amount) {
+			if (source == DamageSource.FALL)
+				return false;
+			if (source == DamageSource.CACTUS)
+				return false;
+			if (source == DamageSource.DROWN)
+				return false;
+			return super.attackEntityFrom(source, amount);
+		}
+
+		@Override
+		protected void applyEntityAttributes() {
+			super.applyEntityAttributes();
+			if (this.getEntityAttribute(SharedMonsterAttributes.ARMOR) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(0D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(10D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3D);
+		}
+
+		public void onLivingUpdate() {
+			super.onLivingUpdate();
+			int i = (int) this.posX;
+			int j = (int) this.posY;
+			int k = (int) this.posZ;
+			Random random = this.rand;
+			if (true)
+				for (int l = 0; l < 4; ++l) {
+					double d0 = (double) ((float) i + 0.5) + (double) (random.nextFloat() - 0.5) * 0.5D;
+					double d1 = ((double) ((float) j + 0.7) + (double) (random.nextFloat() - 0.5) * 0.5D) + 0.5;
+					double d2 = (double) ((float) k + 0.5) + (double) (random.nextFloat() - 0.5) * 0.5D;
+					world.spawnParticle(EnumParticleTypes.HEART, d0, d1, d2, 0, 0, 0);
+				}
+		}
+	}
+}
