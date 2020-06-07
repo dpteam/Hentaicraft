cc7808c78982c8b912597848c07508924644c689
DartPower <SIMPSON-NEKEK@mail.ru>
2020-06-07 21:38:44 +0300

    Added a VulgariumDimension

---
+++ src/main/java/io/alaska/nsfw/item/ItemVulgariumDimension.java
@@ -0,0 +1,42 @@
+
+package io.alaska.nsfw.item;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.creativetab.CreativeTabs;
+
+import io.alaska.nsfw.world.WorldVulgariumDimension;
+
+public class ItemVulgariumDimension extends Item {
+	@GameRegistry.ObjectHolder("nsfw:vulgariumdimension")
+	public static final Item block = null;
+	public ItemVulgariumDimension() {
+		super();
+		this.maxStackSize = 1;
+		setMaxDamage(64);
+		setCreativeTab(CreativeTabs.TOOLS);
+	}
+
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer entity, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY,
+			float hitZ) {
+		pos = pos.offset(facing);
+		ItemStack itemstack = entity.getHeldItem(hand);
+		if (!entity.canPlayerEdit(pos, facing, itemstack)) {
+			return EnumActionResult.FAIL;
+		} else {
+			if (world.isAirBlock(pos))
+				WorldVulgariumDimension.portal.portalSpawn(world, pos);
+			itemstack.damageItem(1, entity);
+			return EnumActionResult.SUCCESS;
+		}
+	}
+}
